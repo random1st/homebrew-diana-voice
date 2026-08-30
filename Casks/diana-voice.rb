0cf04e4c69216d9b9c29314c880638dbd55a4ec2
random1st <voroninr@gmail.com>
2026-08-30 05:58:29 +0100

    diana-voice 0.1.0: setup actions confirm with floating alerts

--- Casks/diana-voice.rb
+++ Casks/diana-voice.rb
@@ -1,6 +1,6 @@
 cask "diana-voice" do
   version "0.1.0"
-  sha256 "a069378affdd41ad1e6c1a23a64f893ad8bc3e42dcdb9f930a2e39b02d4cb291"
+  sha256 "158a4a74d6db3b37490fef078b5a9dbadc38d128751efeabd8688b6c0738fe81"
 
   url "https://github.com/random1st/diana-voice/releases/download/v#{version}/DianaVoice-#{version}.dmg"
   name "Diana Voice"
