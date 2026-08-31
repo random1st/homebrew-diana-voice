f26e5913d73349da40327628d81accb610bedd9b
random1st <voroninr@gmail.com>
2026-08-31 09:15:17 +0100

    diana-voice: hook removed from menu + self-cleanup on launch

--- Casks/diana-voice.rb
+++ Casks/diana-voice.rb
@@ -1,6 +1,6 @@
 cask "diana-voice" do
   version "0.1.0"
-  sha256 "da8fa35794ddd91bc6c6904dd80b545c611f0cd148996d838ca430f33ca24647"
+  sha256 "3ae48e862966e3993af36803fb5145f2c8f8e8a3cd57d16ed57a20c70a7db26e"
 
   url "https://github.com/random1st/diana-voice/releases/download/v#{version}/DianaVoice-#{version}.dmg"
   name "Diana Voice"
